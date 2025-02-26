98d17614962a6c952631444cc31cda7922040d54
unknown <anupshk39@gmail.com>
2025-02-26 23:46:20 +0545

    Build Portfolio For Own Business

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var typed = new Typed('.input',{
+    strings:["Full Stack Developer","Laravel Developer","Vue Developer","React Developer","Wordpress Developer"],
+    typedSpeed:200,
+    backSpeed:100,
+    loop:true
+});
